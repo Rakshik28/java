b67c3825dfe3d4691fd62314feac5a437a474e8f
Rakshitha.K <kurella.rakshitha28@gmail.com>
2025-08-07 18:07:35 +0530

    Add files via upload

---
+++ sumofmrks.java
@@ -0,0 +1,11 @@
+public class sumofmrks {
+    public static void main(String[] args) {
+        int maths = 85;      
+        int physics = 90;
+        int chemistry = 88;
+
+        int total = maths + physics + chemistry;  
+        System.out.println("Total marks: " + total);
+    }
+}
+
