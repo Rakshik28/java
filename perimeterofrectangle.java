9f1940d97ddda0779a1a19a98539cfbac33ef43f
Rakshitha.K <kurella.rakshitha28@gmail.com>
2025-08-07 18:03:17 +0530

    Add files via upload

---
+++ perimeterofrectangle.java
@@ -0,0 +1,8 @@
+public class perimeterofrectangle {
+    public static void main(String[] args) {
+        int length = 5;
+        int width = 3;
+        int perimeter = 2 * (length + width);
+        System.out.println("Perimeter of the rectangle: " + perimeter);
+    }
+}
